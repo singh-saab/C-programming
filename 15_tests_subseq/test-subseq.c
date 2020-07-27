4e8ca761e906f4450a41b957843051e50f7ef524
Coursera Learner <nobody@nowhere.nul>
2020-07-27 04:39:18 +0000

    assg 17 completed

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -9,12 +9,16 @@ int main(){
   int d[]={1,2,-1,18,-3};
   int e[]={4,5,-5,-4 };
   int f[]={0,1,3,38 ,4};
+  int g[]={25,26,27,15};
+  int h[]={12,7,25};
   if (maxSeq(a,10)!=4){ return EXIT_FAILURE;}
   if (maxSeq(b,10)!=1) { return EXIT_FAILURE;}
   if (maxSeq(c,1)!=1) {return EXIT_FAILURE; }
   if (maxSeq(d,5)!=2) {return EXIT_FAILURE;}
  if (maxSeq(e,4)!=2) {return EXIT_FAILURE;}
  if (maxSeq(f,5)!=4) {return EXIT_FAILURE;}
+ if (maxSeq(g,4)!=3) {return EXIT_FAILURE;}
+ if (maxSeq(f,3)!=3) {return EXIT_FAILURE;}
 
   return EXIT_SUCCESS;
 }
