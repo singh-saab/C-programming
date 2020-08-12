371aa07586a565b7fa6e8e30f8871e181df8d3c0
Coursera Learner <nobody@nowhere.nul>
2020-08-12 08:16:58 +0000

    now done

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,59 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
-int calc_frequency(FILE*f, int frequency[])
-{
-  int c;
-  while((c=fgetc(f))!= EOF)
-    {
-      if( isalpha(c))
-	{ c=tolower(c);
-	  c-='a';
-	  frequency[c]++;}
+//Program to break a ceaser cipher using frequency counting
+
+void  frequencyOfChars(FILE * file,int frequencies[]){
+  int currChar;
+  while((currChar = fgetc(file)) != EOF){
+    if(isalpha(currChar)){
+      currChar = tolower(currChar);
+      currChar -= 'a';
+      frequencies[currChar]++;
     }
+  }
 }
 
-int calc_key(int frequency[])
-{
-  int i , max = 0 ,key=0;
-  for ( i=0;i<26;i++ )
-    {
-      if (frequency[i] > max )
-	{ max =i; }
+int calcKey(int frequencies[]){
+  //Loop through the array and find index of largest value
+  int maxIdx = 0;
+  for(int i=0;i<26;i++){
+    if(frequencies[i] > frequencies[maxIdx]){
+      maxIdx = i;
     }
+  }
 
-      if(max>= 4)
-	{key = max - 4;
-	 
-	}
-      if (max<4)
-	{key = 4 - max;
-	}
-      return key;
+  int key = 0;
+
+  if(maxIdx >= ('e'-'a')){
+    key = maxIdx - ('e'-'a');
+  }
+  else {
+    key = (26-('e'-'a')) + maxIdx;
+  }
+
+  return key;
 }
-  
-int main(int argc, char** argv ){
-  if(argc!=2)
-    {
-      fprintf(stderr,"wrong number of arguments");
-      return  EXIT_FAILURE;
-    }
-  FILE*f =fopen(argv[1],"r");
-  if( f== NULL )
-    {
-      perror("file is not valid \n");
-     return EXIT_FAILURE;
-    }
-  int frequency[26] = {0};
-  calc_frequency(f,frequency);
 
-  int key = calc_key(frequency);
+int main(int argc, char ** argv){
+  //Error handling code - checking number of command line arguments
+  if(argc != 2){
+    fprintf(stderr,"Wrong number of command line arguments\n");
+    return EXIT_FAILURE;
+  }
 
-  printf("%d \n",key);
+  //Read in file
+  FILE * file = fopen(argv[1],"r");
 
-  if(fclose(f)!=0)
-    {perror("failed to close the file \n");
-      return EXIT_FAILURE;
-    }
+  //Error handling code - checking file is valid
+  if(file == NULL){
+    perror("File is not valid\n");
+    return EXIT_FAILURE;
+  }
+
+  //Count the frequency of characters
+  int frequencies[26] = {0};
+  frequencyOfChars(file,frequencies);
+
+  //Calculate key from most frequencies
+  int key = calcKey(frequencies);
+
+  printf("%d\n",key);
+
+  //Error handling code - making sure file is closed
+  if(fclose(file) != 0){
+    perror("Failed to close the file\n");
+    return EXIT_FAILURE;
+  }
 }
